943e0b4d711171414abd50e7706733f9143f9a8d
jaswanth6988 <jaswanth123snvk@gmail.com>
2025-04-25 06:41:53 +0530

    Initial commit for Portfolio Website

---
+++ src/Details.js
@@ -0,0 +1,240 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import rust from "./assets/techstack/rust.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+import ovpn from "./assets/techstack/ovpn.png";
+import vim from "./assets/techstack/vim.png";
+import python from "./assets/techstack/python.png";
+import pip from "./assets/techstack/pip.png";
+import cpp from "./assets/techstack/cpp.png";
+import clan from "./assets/techstack/clan.png";
+import java from "./assets/techstack/java.png";
+import cargo from "./assets/techstack/cargo.png";
+import linux from "./assets/techstack/linux.png";
+import mysql1 from "./assets/techstack/mysql1.png";
+import micro from "./assets/techstack/micro.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpeg";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.jpeg";
+import projectImage4 from "./assets/projects/project4.jpeg";
+import projectImage5 from "./assets/projects/project5.jpeg";
+import projectImage6 from "./assets/projects/project6.png";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "JASWANTH SRIGHAKOLLAPU",
+  tagline: "I am a Cybersecurity Explorer | UI/UX Designer",
+  img: profile,
+  about: `I'm N V K Jaswanth Srighakollapu  — I go with a name noPeace among the all popular sites I'm a Cybersecurity Enthusiast, Full Stack Developer, and UI/UX Designer currently pursuing B.Tech in Computer Science at LPU. I build secure, efficient, and user-focused digital experiences by blending technical depth with creative design.
+Proficient in C/C++, Rust, Python, HTML, CSS, JavaScript, React.js, MySQL, and DSA, 
+
+I also bring hands-on knowledge in networking (TCP/IP, DNS), security tools (Wireshark, Nmap), and secure coding practices.
+With a strong grasp of design thinking and prototyping, I craft seamless interfaces. Certified in Cloud Computing, Cyber Incident Response, and Deep Learning, I’m always exploring innovative ways to merge secure backends with intuitive frontends.
+
+`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/n-v-k-jaswanth-srighakollpu-491004251/",
+  github: "https://github.com/jaswanth6988/",
+  twitter: "https://x.com/Jaswant15981967",
+  instagram: "https://www.instagram.com/zneo_47/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Summer Training - DSA",
+    Company: "Board Infinity",
+    Location: "Online",
+    Type: "Training",
+    Duration: "May 2024 - July 2024",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Technology - CSE",
+    Company: "Lovely Professional University",
+    Location: "Punjab, India",
+    Type: "Full Time",
+    Duration: "Aug 2022 - Present",
+  },
+  {
+    Position: "Intermediate (MPC)",
+    Company: "Bhashyam Institute of School",
+    Location: "Guntur, Andhra Pradesh",
+    Type: "Full Time",
+    Duration: "Jun 2019 - Jul 2021",
+  },
+  {
+    Position: "Matriculation",
+    Company: "Bhashyam School",
+    Location: "Est.Gdv, Andhra Pradesh",
+    Type: "Full Time",
+    Duration: "Jun 2018 - Apr 2019",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  rust: rust,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+  ovpn: ovpn,
+  clan: clan,
+  cpp: cpp,
+  mysql1: mysql1,
+  python: python,
+  pip: pip,
+  java: java,
+  cargo: cargo,
+  micro: micro,
+  linux: linux,
+  vim: vim,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Instagram Automation Bot",
+    image: projectImage1,
+    description:
+      "A Selenium-based Python bot automating likes, shares, and comments with proxy rotation and bot detection bypasses.",
+    techstack: "Python, Selenium, BeautifulSoup, Proxy Servers",
+    previewLink: "https://github.com/jaswanth6988/Automation-Instagram-Reels-Sharing/blob/main/README.md",
+    githubLink: "https://github.com/jaswanth6988/Automation-Instagram-Reels-Sharing",
+  },
+  {
+    title: "Library Management System",
+    image: projectImage2,
+    description:
+      "A console-based library system in C++ with book management, using DSA concepts like stacks and queues for performance.",
+    techstack: "C++, DSA",
+    previewLink: "https://github.com/jaswanth6988/PROJECT-REPORT-ON-SIMPLE-LIBRARY-MANAGEMENT-SYSTEM-USING-DSA/blob/main/README.md",
+    githubLink: "https://github.com/jaswanth6988/PROJECT-REPORT-ON-SIMPLE-LIBRARY-MANAGEMENT-SYSTEM-USING-DSA",
+  },
+  {
+    title: "Travel App UI/UX Design",
+    image: projectImage3,
+    description:
+      "Designed a smooth and engaging travel app UI using Figma with user-centric research, reducing design iterations by 40%.",
+    techstack: "Figma, Adobe XD, UI/UX Principles",
+    previewLink: "https://www.figma.com/design/R93msMsDavEjnvWyt2iQO8/high-fedility?node-id=0-1&t=cZ0nqoQE1kwc3Kkd-1",
+    githubLink: "https://www.figma.com/design/R93msMsDavEjnvWyt2iQO8/high-fedility?node-id=0-1&t=cZ0nqoQE1kwc3Kkd-1",
+  },
+  {
+    title: "Enhance Dashboard Security",
+    image: projectImage4,
+    description:
+      "Implemented RBAC to restrict unauthorized dashboard access and secured sensitive data via HTTPS and SSL certificates.",
+    techstack: "Node.js, Express.js, HTTPS, RBAC, SSL",
+    previewLink: "https://github.com/jaswanth6988/Enhance-Dashboard-security/blob/main/README.MD",
+    githubLink: "https://github.com/jaswanth6988/Enhance-Dashboard-security",
+  },
+  {
+    title: "Network Traffic Monitoring with Wireshark",
+    image: projectImage5,
+    description:
+      "Analyzed network traffic to detect ARP spoofing, unauthorized access, and DNS tunneling using Wireshark.",
+    techstack: "Wireshark, TCP/IP, ARP, DNS, Packet Analysis",
+    previewLink: "https://github.com/jaswanth6988/Network-Traffic-Monitoring-Using-Wireshark/blob/main/Readme.MD",
+    githubLink: "https://github.com/jaswanth6988/Network-Traffic-Monitoring-Using-Wireshark",
+  },
+  {
+    title: "Library Bookstore Website",
+    image: projectImage6,
+    description:
+      "A responsive online bookstore built with HTML, CSS, and JavaScript allowing users to browse and purchase books.",
+    techstack: "HTML, CSS, JavaScript",
+    previewLink: "https://jaswanth6988.github.io/TimeFlys_KOC08_Cipherschools/",
+    githubLink: "https://github.com/jaswanth6988/TimeFlys_KOC08_Cipherschools",
+  },
+
+  
+  
+];
+
+export const practicePlatforms = [
+  {
+    name: "LeetCode",
+    link: "https://leetcode.com/u/jaswanthsrighakollapu/",
+    description: "Solved 800+ problems and consistently maintained a rating of 1800+, ranking among the top competitive programmers.",
+  },
+  {
+    name: "TryHackMe",
+    link: "https://tryhackme.com/p/noPeace",
+    description: "Ranked in the Top 5% globally. Completed hands-on labs covering offensive security, networking, and real-world threat analysis.",
+  },
+];
+
+// Blogs
+export const blogs = [
+  {
+    title: "Web 3.0 and the Decentralized Internet",
+    link: "https://medium.com/@noPeace/web-3-0-and-the-decentralized-internet-whats-all-the-buzz-about-15995ee604dd",
+    description: "A beginner's guide to understanding web3 and how it acs and affets.",
+  },
+  {
+    title: "UI/UX Design Principles",
+    link: "https://yourblog.com/ui-ux-design",
+    description: "Learn the key principles of creating user-friendly designs.",
+  },
+];
+
+// Certificates
+export const certificates = [
+  {
+    title: "Cyber incident Response",
+    issuer: "Coursera | Infosec",
+    link: "https://drive.google.com/file/d/1BNKzineWNDIz7PYVSW-90wg_wJIjxu4Q/view",
+  },
+  {
+    title: "Cloud computing",
+    issuer: "NPTEL",
+    link: "https://drive.google.com/file/d/1OCeOBJBwdVmOOnsS5Vt2TymUCzADRGtq/view",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "jaswanth123snvk@gmail.com",
+  phone: "+91 6281276717",
+};
